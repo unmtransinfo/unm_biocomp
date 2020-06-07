60a1c2e611634b197cd8337f595bd7a3c343ed11
Jeremy Yang <jeremyjyang@gmail.com>
2020-06-07 15:35:05 -0600

    Ok.

--- unm_biocomp_sim2d/src/main/java/edu/unm/health/biocomp/sim2d/sim2d.java
+++ unm_biocomp_sim2d/src/main/java/edu/unm/health/biocomp/sim2d/sim2d.java
@@ -5,6 +5,9 @@ import java.lang.reflect.*; //Method
 import java.util.*; //BitSet
 import java.util.concurrent.*; // ExecutorService, Executors
 
+import org.apache.commons.cli.*; // CommandLine, CommandLineParser, HelpFormatter, OptionBuilder, Options, ParseException, PosixParser
+import org.apache.commons.cli.Option.*; // Builder
+
 import chemaxon.formats.*; //MolImporter, MolExporter
 import chemaxon.struc.*; //Molecule, MoleculeGraph
 import chemaxon.descriptors.*; //CFParameters, ECFPParameters
@@ -117,9 +120,9 @@ public class sim2d
   }
 
   /////////////////////////////////////////////////////////////////////////////
-  static void WriteHits(Vector<Sim2DHit> hits, String fpname, Integer n_max_hits, Boolean output_smiles, PrintWriter fout_writer, int verbose) throws Exception
+  static void WriteHits(Vector<Sim2DHit> hits, String fpname, Integer n_max_hits, PrintWriter fout_writer, int verbose) throws Exception
   {
-    fout_writer.write((output_smiles?"SMILES\t":"")+"Name\t"+fpname+"_Similarity\tFP_Is_Subset\n");
+    fout_writer.write("SMILES\tName\t"+fpname+"_Similarity\tFP_Is_Subset\n");
     int i=0;
     for (; i<hits.size(); ++i)
     {
@@ -127,7 +130,7 @@ public class sim2d
       String molname = hits.get(i).name;
       String sim = String.format("%.2f", hits.get(i).sim);
       String subset = hits.get(i).subset ? "1":"0";
-      fout_writer.write((output_smiles?(smi+"\t"):"")+molname+"\t"+sim+"\t"+subset+"\n");
+      fout_writer.write(smi+"\t"+molname+"\t"+sim+"\t"+subset+"\n");
       fout_writer.flush();
       if (i>=n_max_hits) break;
     }
@@ -137,35 +140,6 @@ public class sim2d
   /////////////////////////////////////////////////////////////////////////////
   private static String APPNAME = "SIM2D";
   private static String fptype="path";
-
-  private static void Help(String msg)
-  {
-    System.err.println(msg+"\n"
-      +APPNAME+" - similarity using binary fingerprints\n"
-      +"\n"
-      +"usage: sim2d [options]\n"
-      +"required:\n"
-      +"    -i IFILE ............................. input molecules\n"
-      +"    and\n"
-      +"    -qsmi SMILES ......................... query smiles\n"
-      +"    or\n"
-      +"    -qmol QFILE .......................... query molecule\n"
-      +"\n"
-      +"options:\n"
-      +"    -o OFILE ............................. output hits (TSV)\n"
-      +"    -fptype FPTYPE ....................... fingerprint type (path|ecfp|smartsfile) ["+fptype+"]\n"
-      +"    -smartsfile SMARTSFILE ............... SMARTS file, for -fptype smartsfile, e.g. mdl166.sma, sunsetkeys.sma\n"
-      +"    -n_max NMAX .......................... max db mols\n"
-      +"    -n_max_hits NMAX_HITS ................ max hit mols\n"
-      +"    -min_sim MIN_SIM ..................... minimum similarity\n"
-      +"    -output_smiles ....................... include SMILES in output\n"
-      +"    -v ................................... verbose\n"
-      +"    -h ................................... this help\n"
-      +"\n"
-    );
-    System.exit(1);
-  }
-  private static int verbose=0;
   private static String ifile=null;
   private static String qsmi=null;
   private static String qfile=null;
@@ -174,7 +148,7 @@ public class sim2d
   private static Integer n_max_hits=10;
   private static Integer n_max=null;
   private static Float min_sim=0.0f;
-  private static Boolean output_smiles=false;
+  private static int verbose=0;
 
   // java.util.concurrent.ExecutionException: java.lang.NullPointerException
   // May be due to handling of variable "hits" returned from
@@ -182,32 +156,45 @@ public class sim2d
   private static volatile Vector<Sim2DHit> hits = null;
 
   /////////////////////////////////////////////////////////////////////////////
-  private static void ParseCommand(String args[])
-  {
-    for (int i=0;i<args.length;++i)
-    {
-      if (args[i].equals("-i")) ifile=args[++i];
-      else if (args[i].equals("-o")) ofile=args[++i];
-      else if (args[i].equals("-qsmi")) qsmi=args[++i];
-      else if (args[i].equals("-qmol")) qfile=args[++i];
-      else if (args[i].equals("-fptype")) fptype=args[++i];
-      else if (args[i].equals("-smartsfile")) smartsfile=args[++i];
-      else if (args[i].equals("-min_sim")) min_sim=Float.parseFloat(args[++i]);
-      else if (args[i].equals("-n_max")) n_max=Integer.parseInt(args[++i]);
-      else if (args[i].equals("-n_max_hits")) n_max_hits=Integer.parseInt(args[++i]);
-      else if (args[i].equals("-output_smiles")) output_smiles=true;
-      else if (args[i].equals("-v")) verbose=1;
-      else if (args[i].equals("-h")) Help("");
-      else Help("Unknown option: "+args[i]);
-    }
-  }
-
-  /////////////////////////////////////////////////////////////////////////////
   public static void main(String[] args) throws Exception
   {
-    ParseCommand(args);
+    String HELPHEADER =  "SIM2D: similarity using binary fingerprints";
+    Options opts = new Options();
+    opts.addOption(Option.builder("i").required().hasArg().argName("IFILE").desc("Input file").build());
+    opts.addOption(Option.builder("o").hasArg().argName("OFILE").desc("Output file").build());
+    opts.addOption(Option.builder("qsmi").hasArg().argName("QSMI").desc("Query SMILES").build());
+    opts.addOption(Option.builder("qfile").hasArg().argName("QFILE").desc("Query molecule file").build());
+    opts.addOption(Option.builder("fptype").hasArg().argName("FPTYPE").desc("FP type (path|ecfp|smarts)").build());
+    opts.addOption(Option.builder("smartsfile").hasArg().argName("SMARTSFILE").desc("SMARTS file, for -fptype smarts").build());
+    opts.addOption(Option.builder("min_sim").type(Float.class).hasArg().argName("MIN_SIM").desc("Min similarity").build());
+    opts.addOption(Option.builder("n_max").type(Integer.class).hasArg().argName("N_MAX").desc("Max mols processed").build());
+    opts.addOption(Option.builder("n_max_hits").type(Integer.class).hasArg().argName("N_MAX_HITS").desc("Max hits returned").build());
+    opts.addOption("v", "verbose", false, "Verbose.");
+    opts.addOption("h", "help", false, "Show this help.");
+    HelpFormatter helper = new HelpFormatter();
+    CommandLineParser clip = new PosixParser();
+    CommandLine clic = null;
+    try {
+      clic = clip.parse(opts, args);
+    } catch (ParseException e) {
+      helper.printHelp(APPNAME, HELPHEADER, opts, e.getMessage(), true);
+      System.exit(0);
+    }
+    ifile = clic.getOptionValue("i");
+    if (clic.hasOption("o")) ofile = clic.getOptionValue("o");
+    if (clic.hasOption("qsmi")) qsmi = clic.getOptionValue("qsmi");
+    if (clic.hasOption("qfile")) qfile = clic.getOptionValue("qfile");
+    if (clic.hasOption("smartsfile")) { smartsfile = clic.getOptionValue("smartsfile"); }
+    if (clic.hasOption("n_max")) { n_max = (Integer)(clic.getParsedOptionValue("n_max")); }
+    if (clic.hasOption("n_max_hits")) { n_max_hits = (Integer)(clic.getParsedOptionValue("n_max_hits")); }
+    if (clic.hasOption("min_sim")) { min_sim = (Float)(clic.getParsedOptionValue("min_sim")); }
+    if (clic.hasOption("v")) { verbose = 1; }
+    if (clic.hasOption("h")) {
+      helper.printHelp(APPNAME, HELPHEADER, opts, "", true);
+      System.exit(0);
+    }
+
     if (verbose>0) System.err.println("JRE_VERSION: "+JREUtils.JREVersion());
-    if (ifile==null) Help("Input file required.");
 
     File fout = (ofile!=null) ? (new File(ofile)) : null;
     PrintWriter fout_writer = (fout!=null) ? (new PrintWriter(new BufferedWriter(new FileWriter(fout, false)))) : new PrintWriter(System.out);
@@ -218,11 +205,11 @@ public class sim2d
     }
     else if (qfile!=null) {
       MolImporter molReaderQ = new MolImporter(new File(qfile), "smiles:d");
-      try { molQ = molReaderQ.read(); }
-      catch (MolFormatException e) { Help(e.toString()); }
+      molQ = molReaderQ.read();
     }
     else {
-      Help("Input query required: -qsmi or -qmol.");
+      helper.printHelp(APPNAME, HELPHEADER, opts, "Input query required: -qsmi or -qmol.", true);
+      System.exit(0);
     }
 
     System.err.println("query mol: "+MolExporter.exportToFormat(molQ, "smiles:u")+" "+molQ.getName());
@@ -240,18 +227,22 @@ public class sim2d
     }
     else if (fptype.equalsIgnoreCase("smartsfile")) {
       if (smartsfile==null) {
-        Help("SMARTS file required.");
+       
+        helper.printHelp(APPNAME, HELPHEADER, opts, "SMARTS file required.", true);
+        System.exit(0);
       }
       else if (!(new File(smartsfile)).exists()) {
-        Help("SMARTS file not found: "+smartsfile);
+        helper.printHelp(APPNAME, HELPHEADER, opts, ("SMARTS file not found: "+smartsfile), true);
+        System.exit(0);
       }
       hits = Sim2D_1xN_Smarts_LaunchThread(molReader, smartsfile, (new File(smartsfile)).getName(), molQ, n_max, n_max_hits, min_sim, verbose);
       fptype = (new File(smartsfile)).getName();
     }
     else {
-      Help("Invalid fptype: "+fptype);
+        helper.printHelp(APPNAME, HELPHEADER, opts, ("Invalid fptype: "+fptype), true);
+        System.exit(0);
     }
-    WriteHits(hits, fptype, n_max_hits, output_smiles, fout_writer, verbose);
+    WriteHits(hits, fptype, n_max_hits, fout_writer, verbose);
     fout_writer.close();
     System.exit(0);
   }
